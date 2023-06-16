709db91d53510114fe574ca9c97f5ec48748313c
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-06-16 15:57:31 +0000

    Fix: Reformat JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 const empatheticQuestion = [
-    “Given the choice of anyone in the world, whom would you want as a dinner guest?”,
-    “Would you like to be famous? In what way?”,
-    “Before making a telephone call, do you ever rehearse what you are going to say? Why?”,
-    “What would constitute a 'perfect' day for you?”,
-    “When did you last sing to yourself? To someone else?”,
-    “If you were able to live to the age of 90 and retain either the mind or body of a 30-year-old for the last 60 years of your life, which would you want?”,
-    “Do you have a secret hunch about how you will die?”,
-    “Name three things you and your partner appear to have in common.”,
-    “For what in your life do you feel most grateful?”,
-    “If you could change anything about the way you were raised, what would it be?”,
-    “Take four minutes and tell your partner your life story in as much detail as possible.”,
-    “If you could wake up tomorrow having gained any one quality or ability, what would it be?”,
-    “If a crystal ball could tell you the truth about yourself, your life, the future or anything else, what would you want to know?”,
-    “Is there something that you’ve dreamed of doing for a long time? Why haven’t you done it?”,
-    “What is the greatest accomplishment of your life?”,
-    “What do you value most in a friendship?”,
-    “What is your most treasured memory?”
+    "Given the choice of anyone in the world, whom would you want as a dinner guest?",
+    "Would you like to be famous? In what way?",
+    "Before making a telephone call, do you ever rehearse what you are going to say? Why?",
+    "What would constitute a 'perfect' day for you?",
+    "When did you last sing to yourself? To someone else?",
+    "If you were able to live to the age of 90 and retain either the mind or body of a 30-year-old for the last 60 years of your life, which would you want?",
+    "Do you have a secret hunch about how you will die?",
+    "Name three things you and your partner appear to have in common.",
+    "For what in your life do you feel most grateful?",
+    "If you could change anything about the way you were raised, what would it be?",
+    "Take four minutes and tell your partner your life story in as much detail as possible.",
+    "If you could wake up tomorrow having gained any one quality or ability, what would it be?",
+    "If a crystal ball could tell you the truth about yourself, your life, the future or anything else, what would you want to know?",
+    "Is there something that you’ve dreamed of doing for a long time? Why haven’t you done it?",
+    "What is the greatest accomplishment of your life?",
+    "What do you value most in a friendship?",
+    "What is your most treasured memory?"
 ];
 
 document.getElementById("empatheticQuestion").innerHTML = `${score}`;
